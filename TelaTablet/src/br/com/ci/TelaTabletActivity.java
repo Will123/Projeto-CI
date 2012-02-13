f215988e3a9a336ee496fde75006c64c6b7c1de2
William Tasato <wgtasato@gmail.com>
2012-02-13 11:44:15 -0200

    Primeiro commit

---
+++ TelaTablet/src/br/com/ci/TelaTabletActivity.java
@@ -0,0 +1,13 @@
+package br.com.ci;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TelaTabletActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
